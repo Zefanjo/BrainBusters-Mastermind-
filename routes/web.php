b139da94fd91be7f443dace3d5c2513ab2f3c9b9
Kian de Vries <kvries8501@student.landstede.nl>
2025-01-13 11:29:24 +0100

    container gevoegd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,3 +18,10 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('register');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::post('/start-game', [GameController::class, 'startGame']);
+Route::post('/check-guess', [GameController::class, 'checkGuess']);
