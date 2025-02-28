06f380e21a246003af35ce2e91fb4bd0ca130c85
Kian de Vries <kvries8501@student.landstede.nl>
2025-02-28 11:26:00 +0100

    container gevoegd

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/auth.css','resources/js/auth.js')
-    <title>Profile</title>
+    <title>Profile | Brain Busters</title>
 </head>
 <body>
 
